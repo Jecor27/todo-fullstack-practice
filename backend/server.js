34f13358c380d914ebffc3d3ef0feb01092b0fa0
Jesus <jjcorrales76@gmail.com>
2025-03-05 09:53:43 -0700

    initial connect

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express"
+import cors from 'cors'
 
 const app = express()
 
 
 const port = 8080
-app.get('/', (req, res) => {
+
+app.use(cors())
+
+app.get('/test', (req, res) => {
     res.json('hello (from server)')
 })
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
 function App() {
-  return <></>;
+  async function test() {
+    const response = await fetch("http://localhost:8080/test");
+    const result = await response.json();
+    console.log(result);
+  }
+
+  useEffect(() => {
+    test();
+  }, []);
+
+  return <>Hello World</>;
 }
 
 export default App;
